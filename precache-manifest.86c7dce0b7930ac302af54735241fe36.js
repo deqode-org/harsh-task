b402bc5554e1446dc0c449fca97c8bf40ef7e88c
Sumit Rajput <srajput@deqode.com>
2020-12-16 01:28:27 +0530

    Updates

---
+++ precache-manifest.86c7dce0b7930ac302af54735241fe36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90566e7db3a62e11e9d2de31bcf4e115",
+    "url": "/harsh-task/index.html"
+  },
+  {
+    "revision": "186a0c52677cab5ff9d3",
+    "url": "/harsh-task/static/css/main.a114d3dd.chunk.css"
+  },
+  {
+    "revision": "1710443744ec43090ea4",
+    "url": "/harsh-task/static/js/2.eccaac33.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/harsh-task/static/js/2.eccaac33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "186a0c52677cab5ff9d3",
+    "url": "/harsh-task/static/js/main.708d6378.chunk.js"
+  },
+  {
+    "revision": "5c019b2257c19520ee5b",
+    "url": "/harsh-task/static/js/runtime-main.64862033.js"
+  }
+]);
